4adef290dd672a0b381c3ad7fd56a5d8b37949aa
aniket866 <iamaniketkumarmaner@gmail.com>
2026-04-15 22:45:37 +0530

    contracts

---
+++ contracts/core/DataStorage.sol
@@ -0,0 +1,14 @@
+﻿ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.20;
+
+ contract DataStorage {
+     mapping(uint256 => string) internal data;
+
+     function _setData(uint256 id, string memory value) internal {
+         data[id] = value;
+     }
+
+     function getData(uint256 id) external view returns (string memory) {
+         return data[id];
+     }
+ }
---
+++ contracts/interfaces/IAuth.sol
@@ -0,0 +1,6 @@
+﻿ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.20;
+
+ interface IAuth {
+     function isAdmin(address user) external view returns (bool);
+ }
